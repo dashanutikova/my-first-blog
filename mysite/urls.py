23a8301f1c4e5efd169dbec00544b3fc4fcb26c0
dasha <dashanutikova@gmail.com>
2018-09-28 19:26:23 +0700

    новые шаблоны

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,6 @@ from blog import views
 
 urlpatterns = [
 	url(r'^$', views.post_list, name='post_list'),
-	url(r'^admin/', admin.site.urls)
+	url(r'^admin/', admin.site.urls),
+	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
